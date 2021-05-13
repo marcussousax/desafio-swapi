bdc70c3720451e7befb5264b633f5f9ff7ad4196
Marcus Sousa <marvinsousa83@gmail.com>
2021-05-13 19:22:58 -0300

    Update apiURL

--- src/Containers/App/index.jsx
+++ src/Containers/App/index.jsx
@@ -24,7 +24,7 @@ export default class App extends Component {
 
   static defaultProps = {
     cacheOnStorage: true,
-    apiURL: 'https://swapi.co/api/planets/'
+    apiURL: 'https://swapi.dev/api/planets/'
   };
 
   componentDidMount() {
